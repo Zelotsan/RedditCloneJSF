c6d91ac52d4411970de9220606c73b8e8f6b602a
Marco Leutenegger <marco.leutenegger@gmail.com>
2014-10-15 22:33:23 +0200

    added TimeDifference to index.xhtml

--- src/domain/Post.java
+++ src/domain/Post.java
@@ -20,6 +20,7 @@ public class Post extends PublishedContent implements Serializable{
 	private URL link;
 	private List<Comment> comments;
 	private int votes;
+	private Calendar date;
 	
 	public Post() {
 		comments = Collections.synchronizedList(new ArrayList<Comment>());
@@ -64,5 +65,27 @@ public class Post extends PublishedContent implements Serializable{
 	}
 	public void vote(int i) {
 		vote += i;
+	}
+	
+	public String getTimeDifference() {
+		Calendar current = Calendar.getInstance();
+		current.setTime(new Date());
+
+		if (current.get(Calendar.YEAR) > date.get(Calendar.YEAR))
+			return date.get(Calendar.YEAR) + " years ago";
+		if (current.get(Calendar.MONTH) > date.get(Calendar.MONTH))
+			return date.get(Calendar.MONTH) + " months ago";
+		if (current.get(Calendar.WEEK_OF_MONTH) > date.get(Calendar.WEEK_OF_MONTH))
+			return date.get(Calendar.WEEK_OF_MONTH) + " weeks ago";
+		if (current.get(Calendar.DAY_OF_WEEK) > date.get(Calendar.DAY_OF_WEEK))
+			return date.get(Calendar.DAY_OF_WEEK) + " days ago";
+		if (current.get(Calendar.HOUR) > date.get(Calendar.HOUR))
+			return date.get(Calendar.HOUR) + " hours ago";
+		if (current.get(Calendar.MINUTE) > date.get(Calendar.MINUTE))
+			return date.get(Calendar.MINUTE) + " minutes ago";
+		if (current.get(Calendar.SECOND) > date.get(Calendar.SECOND))
+			return date.get(Calendar.SECOND) + " seconds ago";
+		
+		return "just now";
 	}
 }
