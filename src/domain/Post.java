fcf0de470c690d4b7452e405e9e911273fd7b63a
Dominic Mülhaupt <dominicmh@gmail.com>
2014-10-21 17:57:33 +0200

    Fix getTimeDifference() to actually show difference

--- src/domain/Post.java
+++ src/domain/Post.java
@@ -79,19 +79,19 @@ public class Post extends PublishedContent implements Serializable{
 		current.setTime(new Date());
 
 		if (current.get(Calendar.YEAR) > date.get(Calendar.YEAR))
-			return date.get(Calendar.YEAR) + " years ago";
+			return (current.get(Calendar.YEAR) - date.get(Calendar.YEAR)) + " years ago";
 		if (current.get(Calendar.MONTH) > date.get(Calendar.MONTH))
-			return date.get(Calendar.MONTH) + " months ago";
+			return (current.get(Calendar.MONTH) - date.get(Calendar.MONTH)) + " months ago";
 		if (current.get(Calendar.WEEK_OF_MONTH) > date.get(Calendar.WEEK_OF_MONTH))
-			return date.get(Calendar.WEEK_OF_MONTH) + " weeks ago";
+			return (current.get(Calendar.WEEK_OF_MONTH) - date.get(Calendar.WEEK_OF_MONTH)) + " weeks ago";
 		if (current.get(Calendar.DAY_OF_WEEK) > date.get(Calendar.DAY_OF_WEEK))
-			return date.get(Calendar.DAY_OF_WEEK) + " days ago";
+			return (current.get(Calendar.DAY_OF_WEEK) - date.get(Calendar.DAY_OF_WEEK)) + " days ago";
 		if (current.get(Calendar.HOUR) > date.get(Calendar.HOUR))
-			return date.get(Calendar.HOUR) + " hours ago";
+			return (current.get(Calendar.HOUR) - date.get(Calendar.HOUR)) + " hours ago";
 		if (current.get(Calendar.MINUTE) > date.get(Calendar.MINUTE))
-			return date.get(Calendar.MINUTE) + " minutes ago";
+			return (current.get(Calendar.MINUTE) - date.get(Calendar.MINUTE)) + " minutes ago";
 		if (current.get(Calendar.SECOND) > date.get(Calendar.SECOND))
-			return date.get(Calendar.SECOND) + " seconds ago";
+			return (current.get(Calendar.SECOND) - date.get(Calendar.SECOND)) + " seconds ago";
 		
 		return "just now";
 	}
